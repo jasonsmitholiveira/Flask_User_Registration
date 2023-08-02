2a8b521976877a873c0f4af2bcd4afa6961d8a21
Jose Calazans Abrantes Junior <130673741+joseabrantesjr@users.noreply.github.com>
2023-08-01 21:09:50 -0300

    update

--- app.py
+++ app.py
@@ -10,60 +10,73 @@ from wtforms import BooleanField
 import requests
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.urandom(16)  # Generate a random secret key
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
-app.config['UPLOAD_FOLDER'] = 'uploads'  # Add the folder to store uploaded files
+app.config['SECRET_KEY'] = os.urandom(16)  # Gerar uma chave secreta aleatória
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'  # Configurar o URI do banco de dados
+app.config['UPLOAD_FOLDER'] = 'uploads'  # Adicionar a pasta para armazenar os arquivos enviados
 db = SQLAlchemy(app)
 
+# Classificações de Gênero
+GENERO_CHOICES = [('', ''), ('masculino', 'Masculino'), ('feminino', 'Feminino'), ('outro', 'Outro')]
+
+# Classificações de Estado Civil
+ESTADO_CIVIL_CHOICES = [('', ''), ('solteiro', 'Solteiro'), ('casado', 'Casado'), ('divorciado', 'Divorciado'), ('viuvo', 'Viúvo')]
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    cpf_cnpj = db.Column(db.String(20), nullable=False, unique=True)
-    nome_completo = db.Column(db.String(100), nullable=False)
-    data_nascimento = db.Column(db.Date, nullable=False)
-    genero = db.Column(db.String(10))
-    estado_civil = db.Column(db.String(20))
-    email = db.Column(db.String(100), nullable=False, unique=True)
-    telefone = db.Column(db.String(20), nullable=False)
-    senha = db.Column(db.String(100), nullable=False)
-    endereco_cep = db.Column(db.String(10))
-    endereco_logradouro = db.Column(db.String(100))
-    endereco_numero = db.Column(db.String(10))
-    endereco_complemento = db.Column(db.String(50))
-    endereco_bairro = db.Column(db.String(50))
-    endereco_cidade = db.Column(db.String(50))
-    endereco_estado = db.Column(db.String(50))
-    identificacao_arquivo = db.Column(db.String(100))
+    cpf_cnpj = db.Column(db.String(14), nullable=False, unique=True)  # CPF/CNPJ do usuário (único e obrigatório)
+    nome_completo = db.Column(db.String(100), nullable=False)  # Nome completo do usuário (obrigatório)
+    data_nascimento = db.Column(db.Date, nullable=False)  # Data de nascimento do usuário (obrigatório)
+    genero = db.Column(db.String(10))  # Gênero do usuário
+    estado_civil = db.Column(db.String(20))  # Estado civil do usuário
+    email = db.Column(db.String(100), nullable=False, unique=True)  # Email do usuário (único e obrigatório)
+    telefone = db.Column(db.String(11), nullable=False)  # Número de telefone do usuário (obrigatório)
+    senha = db.Column(db.String(100), nullable=False)  # Senha do usuário (obrigatório)
+    endereco_cep = db.Column(db.String(10))  # CEP do endereço do usuário
+    endereco_logradouro = db.Column(db.String(100))  # Logradouro do endereço do usuário
+    endereco_numero = db.Column(db.String(10))  # Número do endereço do usuário
+    endereco_complemento = db.Column(db.String(50))  # Complemento do endereço do usuário
+    endereco_bairro = db.Column(db.String(50))  # Bairro do endereço do usuário
+    endereco_cidade = db.Column(db.String(50))  # Cidade do endereço do usuário
+    endereco_estado = db.Column(db.String(50))  # Estado do endereço do usuário
+    identificacao_arquivo = db.Column(db.String(100))  # Nome do arquivo de identificação do usuário
 
 
 class CadastroForm(FlaskForm):
-    cpf_cnpj = StringField('CPF/CNPJ', validators=[validators.InputRequired()])
-    nome_completo = StringField('Nome Completo', validators=[validators.InputRequired()])
-    data_nascimento = DateField('Data de Nascimento', format='%Y-%m-%d', validators=[validators.InputRequired()])
-    genero = SelectField('Gênero', choices=[('', ''), ('masculino', 'Masculino'), ('feminino', 'Feminino')])
-    estado_civil = SelectField('Estado Civil', choices=[('', ''), ('solteiro', 'Solteiro'), ('casado', 'Casado')])
-    email = StringField('E-mail', validators=[validators.InputRequired(), validators.Email()])
-    telefone = StringField('Telefone', validators=[validators.InputRequired()])
-    senha = PasswordField('Senha', validators=[validators.InputRequired(), validators.EqualTo('confirmar_senha', 'As senhas devem ser iguais.')])
-    confirmar_senha = PasswordField('Confirmar Senha', validators=[validators.InputRequired()])
-    endereco_cep = StringField('CEP', validators=[validators.InputRequired()])
-    endereco_logradouro = StringField('Logradouro', validators=[validators.InputRequired()])
-    endereco_numero = StringField('Número', validators=[validators.InputRequired()])
-    endereco_complemento = StringField('Complemento', validators=[validators.InputRequired()])
-    endereco_bairro = StringField('Bairro', validators=[validators.InputRequired()])
-    endereco_cidade = StringField('Cidade', validators=[validators.InputRequired()])
-    endereco_estado = StringField('Estado', validators=[validators.InputRequired()])
-    identificacao = FileField('Identificação', validators=[validators.InputRequired()])
-    politica = BooleanField('Aceito os termos e condições ao realizar o cadastro.',
-                            validators=[validators.InputRequired()])
+    # Formulário para cadastro do usuário
+    cpf_cnpj = StringField('CPF/CNPJ', validators=[
+        validators.InputRequired(),
+        validators.Length(min=11, max=14, message="CPF/CNPJ deve ter 11 ou 14 números.")
+    ])  # Campo para CPF/CNPJ (obrigatório)
+    nome_completo = StringField('Nome Completo', validators=[validators.InputRequired()])  # Campo para nome completo (obrigatório)
+    data_nascimento = DateField('Data de Nascimento', format='%Y-%m-%d', validators=[validators.InputRequired()])  # Campo para data de nascimento (obrigatório)
+    genero = SelectField('Gênero', choices=GENERO_CHOICES)  # Campo para selecionar gênero
+    estado_civil = SelectField('Estado Civil', choices=ESTADO_CIVIL_CHOICES)  # Campo para selecionar estado civil
+    email = StringField('E-mail', validators=[validators.InputRequired(), validators.Email()])  # Campo para email (obrigatório e validado como email)
+    telefone = StringField('Telefone', validators=[
+        validators.InputRequired(),
+        validators.Length(min=11, max=11, message="Telefone deve conter 11 números (DDD + número do telefone, juntos).")
+    ])  # Campo para número de telefone (obrigatório)
+    senha = PasswordField('Senha', validators=[validators.InputRequired(), validators.EqualTo('confirmar_senha', 'As senhas devem ser iguais.')])  # Campo para senha (obrigatório) e validação de confirmação de senha
+    confirmar_senha = PasswordField('Confirmar Senha', validators=[validators.InputRequired()])  # Campo para confirmar senha (obrigatório)
+    endereco_cep = StringField('CEP', validators=[validators.InputRequired()])  # Campo para CEP (obrigatório)
+    endereco_logradouro = StringField('Logradouro', validators=[validators.InputRequired()])  # Campo para logradouro (obrigatório)
+    endereco_numero = StringField('Número', validators=[validators.InputRequired()])  # Campo para número do endereço (obrigatório)
+    endereco_complemento = StringField('Complemento', validators=[validators.InputRequired()])  # Campo para complemento do endereço (obrigatório)
+    endereco_bairro = StringField('Bairro', validators=[validators.InputRequired()])  # Campo para bairro do endereço (obrigatório)
+    endereco_cidade = StringField('Cidade', validators=[validators.InputRequired()])  # Campo para cidade do endereço (obrigatório)
+    endereco_estado = StringField('Estado', validators=[validators.InputRequired()])  # Campo para estado do endereço (obrigatório)
+    identificacao = FileField('Identificação', validators=[validators.InputRequired()])  # Campo para arquivo de identificação (obrigatório)
+    politica = BooleanField('Aceito os termos e condições ao realizar o cadastro.', validators=[validators.InputRequired()])  # Campo para aceitar os termos e condições ao realizar o cadastro (obrigatório)
 
     def fetch_address_data(cep):
+        # Função para buscar dados de endereço com base no CEP
         url = f'https://viacep.com.br/ws/{cep}/json/'
         response = requests.get(url)
         data = response.json()
         return data
 
     def validate_email(self, field):
+        # Função para validar se o email já está cadastrado no banco de dados
         if User.query.filter_by(email=field.data).first():
             raise validators.ValidationError('E-mail já cadastrado.')
         pass
@@ -72,6 +85,7 @@ class CadastroForm(FlaskForm):
 ALLOWED_EXTENSIONS = {'pdf', 'png', 'jpg', 'jpeg'}
 
 def allowed_file(filename):
+    # Função para verificar se o arquivo possui uma extensão permitida
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/', methods=['GET', 'POST'])
@@ -79,7 +93,7 @@ def cadastro_usuario():
     form = CadastroForm()
 
     if request.method == 'POST' and form.validate_on_submit():
-        # Hash the password before storing it
+        # Hash da senha antes de armazená-la
         hashed_password = generate_password_hash(form.senha.data)
 
         novo_usuario = User(
@@ -100,13 +114,13 @@ def cadastro_usuario():
             endereco_estado=form.endereco_estado.data,
         )
 
-        # Handle the uploaded file
+        # Lidar com o arquivo enviado
         if form.identificacao.data and allowed_file(form.identificacao.data.filename):
             filename = secure_filename(form.identificacao.data.filename)
             form.identificacao.data.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             novo_usuario.identificacao_arquivo = filename
 
-        # Check if the CPF/CNPJ and email are already registered
+        # Verificar se o CPF/CNPJ e o email já estão registrados
         existing_user_by_cpf = User.query.filter_by(cpf_cnpj=form.cpf_cnpj.data).first()
         existing_user_by_email = User.query.filter_by(email=form.email.data).first()
 
