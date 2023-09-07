a9d671c0a3bad8630410c1291cdb78a1825e2f57
jasonsmitholiveira <jasonsmitholiveira@gmail.com>
2023-09-06 22:45:59 -0300

    insert api for auth

--- app.py
+++ app.py
@@ -1,43 +1,51 @@
-import os
-from flask import Flask, render_template, request, flash
+from flask import Flask, render_template, request, flash, redirect, url_for, session
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, validators
+from authlib.integrations.flask_client import OAuth
 from werkzeug.security import generate_password_hash
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 app = Flask(__name__)
 
-# Configura a URL do banco de dados SQLite
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///seu_banco_de_dados.db'
-app.config['SECRET_KEY'] = 'sua_chave_secreta_aqui'  # Defina sua chave secreta aqui
-db = SQLAlchemy(app)
+class CadastroForm(FlaskForm):
+    email = StringField('Email', [validators.DataRequired(), validators.Email()])
+    senha = PasswordField('Senha', [validators.DataRequired(), validators.Length(min=6)])
+    confirmar_senha = PasswordField('Confirmar Senha', [
+        validators.DataRequired(),
+        validators.EqualTo('senha', message='Senhas devem ser iguais')
+    ])
 
+# Auth0 configuration
+app.config['SECRET_KEY'] = 'sua_chave_secreta_aqui'  # Your secret key here
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///seu_banco_de_dados.db'
+app.config['SESSION_TYPE'] = 'filesystem'  # Required for storing user session data
+app.config['AUTH0_CLIENT_ID'] = 'your_auth0_client_id'
+app.config['AUTH0_CLIENT_SECRET'] = 'your_auth0_client_secret'
+app.config['AUTH0_DOMAIN'] = 'your_auth0_domain'
+
+oauth = OAuth(app)
+
+auth0 = oauth.register(
+    'auth0',
+    client_id=app.config['AUTH0_CLIENT_ID'],
+    client_secret=app.config['AUTH0_CLIENT_SECRET'],
+    authorize_url='https://{}/authorize'.format(app.config['AUTH0_DOMAIN']),
+    authorize_params=None,
+    authorize_kwargs=None,
+    access_token_url='https://{}/oauth/token'.format(app.config['AUTH0_DOMAIN']),
+    access_token_params=None,
+    access_token_kwargs=None,
+    refresh_token_url=None,
+    redirect_uri='http://localhost:8000/callback',
+    client_kwargs={'scope': 'openid profile email'},
+)
+
+db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-# Define o modelo de usuário
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(100), nullable=False, unique=True)
-    senha = db.Column(db.String(100), nullable=False)
+# ... The rest of your code ...
 
-# Define o formulário de cadastro
-class CadastroForm(FlaskForm):
-    email = StringField('E-mail', validators=[validators.InputRequired(), validators.Email()])
-    senha = PasswordField('Senha', validators=[validators.InputRequired(), validators.EqualTo('confirmar_senha', message='As senhas devem ser iguais.')])
-    confirmar_senha = PasswordField('Confirmar Senha', validators=[validators.InputRequired()])
-
-    def validate_email(self, field):
-        if User.query.filter_by(email=field.data).first():
-            raise validators.ValidationError('E-mail já cadastrado.')
-
-# Verifica as extensões de arquivo permitidas
-ALLOWED_EXTENSIONS = {'pdf', 'png', 'jpg', 'jpeg'}
-
-def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-# Rota para o cadastro de usuário
 @app.route('/', methods=['GET', 'POST'])
 def cadastro_usuario():
     form = CadastroForm()
@@ -62,10 +70,28 @@ def cadastro_usuario():
 
     return render_template('cadastro2.html', form=form)
 
-# Rota para login (você pode adicionar a funcionalidade de login aqui)
-@app.route('/login', methods=['GET'])
+@app.route('/login')
 def login():
-    return render_template('login.html')
+    return auth0.authorize_redirect(redirect_uri='http://localhost:8000/callback')
+
+@app.route('/callback')
+def callback():
+    auth0.authorize_access_token()
+    response = auth0.get('userinfo')
+    userinfo = response.json()
+
+    # You can use userinfo to manage the user session, store user data, and more.
+
+    return redirect(url_for('index'))
+
+@app.route('/logout')
+def logout():
+    session.clear()  # Clear the user session
+    params = {
+        'returnTo': url_for('index', _external=True),
+        'client_id': app.config['AUTH0_CLIENT_ID'],
+    }
+    return redirect(auth0.api_base_url + '/v2/logout?' + urlencode(params))
 
 if __name__ == '__main__':
     with app.app_context():
